54ae6f7354e5d9eb06673f422f634e0715f13002
Diaz Abdul <67885615+diazabdul@users.noreply.github.com>
2022-08-30 20:50:49 +0700

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("192.168.138.177", 8001);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter Username : ");
+                String username = Console.ReadLine();
+                Console.Write("Enter Password : ");
+                String password = Console.ReadLine();
+                String str = username.Length.ToString() + username + password.Length.ToString() + password;
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Aplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("192.168.138.177");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                
+                int k = s.Receive(b);
+
+
+                char z = Convert.ToChar(b[0]);
+                
+                int uname = (int)char.GetNumericValue(z);
+                char x = Convert.ToChar(b[uname+1]);
+                int passw = (int)char.GetNumericValue(x);
+
+                Console.WriteLine("Recieved...");
+                Console.Write("Username :   ");
+                for (int i = 1; i < uname+1; i++)
+                {
+
+                    Console.Write(Convert.ToChar(b[i]));
+                   
+                }
+                Console.WriteLine();
+                //validasi
+                int validation;
+                if (k-uname <= 9)
+                {
+                    validation = uname + 2;
+                }
+                else
+                {
+                    validation = uname + 3;
+                }
+                
+
+                //
+                Console.Write("Password :   ");
+                for (int j = validation; j <k; j++)
+                {
+                    Console.Write(Convert.ToChar(b[j]));
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Keseluruhan data yang di Passing");
+                for(int o = 0; o < k; o++)
+                {
+                    Console.Write(Convert.ToChar(b[o]));
+                }
+
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The string was recieved by the server."));
+                
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            Console.ReadKey();
+        }
+    }
+}
